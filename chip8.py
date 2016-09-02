3aaef835988b58d2b8a1dafa7bb5418ef988447f
Didriksson <mattias.didriksson@gmail.com>
2016-09-02 22:53:30 +0200

    Implemented OP-codes:
    3XNN, 4XNN, 5XY0, 6XNN, 7XNN,
    8XY0 to 8XYE.

--- chip8.py
+++ chip8.py
@@ -9,20 +9,121 @@ class chip8(object):
         self.memory = bytearray(4096)
         self.opcodes = {
             0x1: self.jumpToAddress,
-            0x3: self.skipNextInstructionIfVXEqualsNN
+            0x3: self.skipNextInstructionIfVXEqualsNN,
+            0x4: self.skipNextInstructionIfVXDoesNotEqualsNN,
+            0x5: self.skipNextInstructionIfVXEqualsVY,
+            0x6: self.setVXToNN,
+            0x7: self.addVXToNN,
+            0x8: self.logicalOperation
         }
+
+        self.logicalOperations = {
+            0x0: self.setVXToVY,
+            0x1: self.setVXToVXorVY,
+            0x2: self.setVXToVXandVY,
+            0x3: self.setVXToVXxorVY,
+            0x4: self.addVYToVX,
+            0x5: self.subtractVYFromVX,
+            0x6: self.shiftVXRight,
+            0x7: self.subtractVXFromVYPlaceInVX,
+            0xE: self.shiftVXLeft
+        }
+
+    def logicalOperation(self, operand):
+        self.logicalOperations[operand & 0x000F](operand)
 
     def jumpToAddress(self, operand):
         self.PC = operand & 0x0FFF
 
     def skipNextInstructionIfVXEqualsNN(self, operand):
-        pass
+        registerToCheck = operand >> 8 & 0xF
+        if self.registers[registerToCheck] == (operand & 0xFF):
+            self.PC += 2
+
+    def skipNextInstructionIfVXDoesNotEqualsNN(self, operand):
+        registerToCheck = operand >> 8 & 0xF
+        if self.registers[registerToCheck] != (operand & 0xFF):
+            self.PC += 2
+
+    def skipNextInstructionIfVXEqualsVY(self, operand):
+        register1 = operand >> 8 & 0xF
+        register2 = operand >> 4 & 0xF
+        if self.registers[register1] == self.registers[register2]:
+            self.PC += 2
+
+    def setVXToNN(self, operand):
+        register = operand >> 8 & 0xF
+        value = operand & 0xFF
+        self.registers[register] = value
+
+    def addVXToNN(self, operand):
+        register = operand >> 8 & 0xF
+        value = operand & 0xFF
+        self.registers[register] += value
+
+    def setVXToVY(self, operand):
+        register1 = operand >> 8 & 0xF
+        register2 = operand >> 4 & 0xF
+        self.registers[register1] = self.registers[register2]
+
+    def setVXToVXorVY(self, operand):
+        reg1 = operand >> 8 & 0xF
+        reg2 = operand >> 4 & 0xF
+        self.registers[reg1] = self.registers[reg1] | self.registers[reg2]
+
+    def setVXToVXandVY(self, operand):
+        reg1 = operand >> 8 & 0xF
+        reg2 = operand >> 4 & 0xF
+        self.registers[reg1] = self.registers[reg1] & self.registers[reg2]
+
+    def setVXToVXxorVY(self, operand):
+        reg1 = operand >> 8 & 0xF
+        reg2 = operand >> 4 & 0xF
+        self.registers[reg1] = self.registers[reg1] ^ self.registers[reg2]
+
+    def addVYToVX(self, operand):
+        reg1 = operand >> 8 & 0xF
+        reg2 = operand >> 4 & 0xF
+        sum = self.registers[reg1] + self.registers[reg2]
+        if sum > 255:
+            sum -= 255
+            self.registers[0xF] = 1
+        self.registers[reg1] = sum
+
+    def subtractVYFromVX(self, operand):
+        reg1 = operand >> 8 & 0xF
+        reg2 = operand >> 4 & 0xF
+        difference = self.registers[reg1] - self.registers[reg2]
+        if difference < 0:
+            difference += 255
+            self.registers[0xF] = 1
+        self.registers[reg1] = difference
+
+    def subtractVXFromVYPlaceInVX(self, operand):
+        reg1 = operand >> 8 & 0xF
+        reg2 = operand >> 4 & 0xF
+        difference = self.registers[reg2] - self.registers[reg1]
+        if difference < 0:
+            difference += 255
+            self.registers[0xF] = 1
+        self.registers[reg1] = difference
+
+    def shiftVXRight(self, operand):
+        reg1 = operand >> 8 & 0xF
+        self.registers[0xF] = self.registers[reg1] & 0x0001
+        self.registers[reg1] = self.registers[reg1] >> 1
+
+    def shiftVXLeft(self, operand):
+        reg1 = operand >> 8 & 0xF
+        self.registers[0xF] = (self.registers[reg1] & 0x80) >> 7
+        self.registers[reg1] = self.registers[reg1] << 1 & 0xFF
+
     def executeInstruction(self, opcode=None):
         if opcode:
             self.opcode = opcode
         else:
             self.opcode = self.memory[self.PC] << 8
             self.opcode += self.memory[self.PC + 1]
-            self.PC += 2
+        self.PC += 2
         operation = (self.opcode & 0xF000) >> 12
         self.opcodes[operation](self.opcode)
--- chip8_test.py
+++ chip8_test.py
@@ -22,8 +22,139 @@ class chip8_test(unittest.TestCase):
         cpu.registers[0xA] = 0xFF
         self.assertEqual(cpu.registers[0xA], 0xFF)
         self.assertEqual(cpu.PC, 0)
-        cpu.executeInstruction(0x30FF)
+        cpu.executeInstruction(0x3AFF)
         self.assertEqual(cpu.PC, 4)
+        cpu.executeInstruction(0x3AFA)
+        self.assertEqual(cpu.PC, 6)
+
+    # 4XNN
+    def testInstructionSkipsTheNextInstructionIfVXDoesNotEqualsNN(self):
+        cpu = chip8()
+        cpu.PC = 0
+        cpu.registers[0xA] = 0xFF
+        self.assertEqual(cpu.registers[0xA], 0xFF)
+        self.assertEqual(cpu.PC, 0)
+        cpu.executeInstruction(0x4AFF)
+        self.assertEqual(cpu.PC, 2)
+        cpu.executeInstruction(0x4AFA)
+        self.assertEqual(cpu.PC, 6)
+
+    # 5XY0
+    def testSkipNextInstructionIfVXEqualsVY(self):
+        cpu = chip8()
+        cpu.PC = 0
+        cpu.registers[0xA] = 0xFF
+        self.assertEqual(cpu.registers[0xA], 0xFF)
+        self.assertEqual(cpu.PC, 0)
+        cpu.executeInstruction(0x5AF0)
+        self.assertEqual(cpu.PC, 2)
+        cpu.registers[0xF] = 0xFF
+        self.assertEqual(cpu.registers[0xF], 0xFF)
+        cpu.executeInstruction(0x5AF0)
+        self.assertEqual(cpu.PC, 6)
+
+    # 6XNN
+    def testSetValueToVX(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0x0
+        self.assertEqual(cpu.registers[0xA], 0x0)
+        cpu.executeInstruction(0x6AFA)
+        self.assertEqual(cpu.registers[0xA], 0xFA)
+
+    # 7XNN
+    def testAddValueToVX(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0x5
+        self.assertEqual(cpu.registers[0xA], 0x5)
+        cpu.executeInstruction(0x7A05)
+        self.assertEqual(cpu.registers[0xA], 0xA)
+
+    # 8XY0
+    def testSetVXToVY(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0x0
+        cpu.registers[0xF] = 0xFA
+        self.assertEqual(cpu.registers[0xA], 0x0)
+        cpu.executeInstruction(0x8AF0)
+        self.assertEqual(cpu.registers[0xA], 0xFA)
+
+    # 8XY1
+    def testSetVXToVXorVY(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0xC
+        cpu.registers[0xF] = 0x3
+        cpu.executeInstruction(0x8AF1)
+        self.assertEqual(cpu.registers[0xA], 0xF)
+
+    # 8XY2
+    def testSetVXToVXandVY(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0x7
+        cpu.registers[0xF] = 0x3
+        cpu.executeInstruction(0x8AF2)
+        self.assertEqual(cpu.registers[0xA], 0x3)
+
+    # 8XY3
+    def testSetVXToVXxorVY(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0x2
+        cpu.registers[0xF] = 0xA
+        cpu.executeInstruction(0x8AF3)
+        self.assertEqual(cpu.registers[0xA], 0x8)
+
+    # 8XY4
+    def testAddVYToVXWithCarry(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0xFF
+        cpu.registers[0xE] = 0xFA
+        cpu.executeInstruction(0x8AE4)
+        self.assertEqual(cpu.registers[0xA], 0xFA)
+        self.assertEqual(cpu.registers[0xF], 0x1)
+
+    # 8XY5
+    def testSubtractVYFromVYWithCarry(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0xA
+        cpu.registers[0xE] = 0xF
+        cpu.executeInstruction(0x8AE5)
+        self.assertEqual(cpu.registers[0xA], 0xFA)
+        self.assertEqual(cpu.registers[0xF], 0x1)
+
+    # 8XY6
+    def testShiftVXRight(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0x3
+        cpu.executeInstruction(0x8AE6)
+        self.assertEqual(cpu.registers[0xA], 0x1)
+        self.assertEqual(cpu.registers[0xF], 0x1)
+
+        cpu.registers[0xA] = 0x2
+        cpu.executeInstruction(0x8AE6)
+        self.assertEqual(cpu.registers[0xA], 0x1)
+        self.assertEqual(cpu.registers[0xF], 0x0)
+
+    # 8XY7
+    def testSetVXToSubtractionOfVXFromVYWithCarry(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0xF
+        cpu.registers[0xE] = 0xA
+        cpu.executeInstruction(0x8AE7)
+        self.assertEqual(cpu.registers[0xA], 0xFA)
+        self.assertEqual(cpu.registers[0xF], 0x1)
+
+    # 8XYE
+    def testShiftVXLeft(self):
+        cpu = chip8()
+        cpu.registers[0xA] = 0x8A
+        cpu.executeInstruction(0x8AEE)
+        self.assertEqual(cpu.registers[0xA], 0x14)
+        self.assertEqual(cpu.registers[0xF], 0x1)
+
+        cpu.registers[0xA] = 0x2
+        cpu.executeInstruction(0x8AEE)
+        self.assertEqual(cpu.registers[0xA], 0x4)
+        self.assertEqual(cpu.registers[0xF], 0x0)
+
 
 if __name__ == '__main__':
     unittest.main()
